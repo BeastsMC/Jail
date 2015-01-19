b3f855e4419ce59ef8933fe62f1e3682087506d0
BeastsMC <beastsmc@gmail.com>
2015-01-19 13:12:45 -1000

    Create base for a callback when a prisoner logs in

--- Jail/src/com/BeastsMC/jail/PrisonerLoginCallback.java
+++ Jail/src/com/BeastsMC/jail/PrisonerLoginCallback.java
@@ -2,7 +2,9 @@ package com.BeastsMC.jail;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import java.sql.SQLException;
 import java.util.UUID;
 
 /**
@@ -16,6 +18,26 @@ public class PrisonerLoginCallback {
     }
 
     public void handle(Prisoner loadedPrisoner) {
+        if (loadedPrisoner.isDirty()) {
+            Player player = Bukkit.getPlayer(pid);
+            if (player == null) {
+                JailPlugin.instance.getLogger().severe("ERROR! Logged in player was NULL");
+                return;
+            }
+            final String[] inv = CommonFunctions.playerInventoryToBase64(player.getInventory());
 
+            JailPlugin.instance.getServer().getScheduler().runTaskAsynchronously(JailPlugin.instance,
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                JailPlugin.instance.getMysql().saveInventory(pid.toString(), inv);
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+            );
+        }
     }
 }
